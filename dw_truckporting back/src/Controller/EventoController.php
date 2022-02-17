cfcc685281a0dc357db5138d5d9e5d9392b1df1d
Mikel alvarez <mikelalvarez271@gmail.com>
2022-02-17 13:09:21 +0100

    commit 17/2/2022

--- dw_truckporting back/src/Controller/EventoController.php
+++ dw_truckporting back/src/Controller/EventoController.php
@@ -8,10 +8,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route ("/evento")
+ */
 class EventoController extends AbstractController
 {
     /**
-     * @Route("/evento", name="evento")
+     * @Route("/", name="evento")
      */
     public function index(): Response
     {
@@ -39,7 +42,81 @@ class EventoController extends AbstractController
         ]);
     }
     /**
-     * @Route("/evento/{id}", name="eventoGetById")
+     * @Route ("/new", name="eventoNew")
+     * @Method({"POST"})
+     */
+    public function eventoNew(Request $request): Response{
+      $dotrine =  $this->getDoctrine()->getManager();
+
+      $evento = new Evento();
+
+      $evento->setNombre($request->get("nombre"));
+      $evento->setLugar($request->get("lugar"));
+      $evento->setFechaInicio($request->get("fechaInicio"));
+      $evento->setFechaFinal($request->get("fechaFinal"));
+      $evento->setImagen($request->get("imagen"));
+
+      $dotrine->persist($evento);
+      $dotrine->flush();
+
+      return $this->json([
+          "message" => "Evento Añadido"
+      ]);
+
+
+    }
+
+    /**
+     * @Route ("/{id}", name="eventoDelete")
+     * @Method ({"DELETE"})
+     */
+    public function eventoDelete($id):Response{
+
+        $doctrine = $this->getDoctrine()->getManager();
+
+        $evento = $doctrine->getRepository(Evento::class)->find($id);
+
+        $doctrine->remove($evento);
+        $doctrine->flush();
+
+        return $this->json([
+            "message"=> "Evento Eliminado"
+        ]);
+    }
+
+    /**
+     * @Route ("/{id}",name = "eventoUpdate")
+     * @Method  ({"PUT"})
+     */
+    public function evnetoUpdate($id,Request $request):Response{
+        $doctrine = $this->getDoctrine()->getManager();
+
+
+
+        $evento = $doctrine->getRepository(Evento::class)->find($request->get("id"));
+        if ($evento == null){
+            return $this->json([
+                "message"=> "Evento no existe"
+            ]);
+        }
+
+
+        $evento->setNombre($request->get("nombre"));
+        $evento->setLugar($request->get("lugar"));
+        $evento->setFechaInicio($request->get("fechaInicio"));
+        $evento->setFechaFinal($request->get("fechaFinal"));
+        $evento->setImagen($request->get("imagen"));
+
+        $doctrine->persist($evento);
+        $doctrine->flush();
+
+        return $this->json([
+            "message"=> "Evento Actualizado"
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="eventoGetById", methods={"GET"})
      */
     public function eventoGetById($id){
         $evento = $this->getDoctrine()->getRepository(Evento::class)->find($id);
@@ -61,4 +138,6 @@ class EventoController extends AbstractController
             $data
         ]);
     }
+
+
 }
