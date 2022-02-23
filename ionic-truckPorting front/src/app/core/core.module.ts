cb278f272d9c952c040b3ba5030af22cd16f8755
Eneko Trigo <02enekotrigo@gmail.com>
2022-02-23 16:56:13 +0100

    unos cambios en el core.omules y auth.service  para ver sis e soluciona el ususario (pero no)

--- ionic-truckPorting front/src/app/core/core.module.ts
+++ ionic-truckPorting front/src/app/core/core.module.ts
@@ -4,10 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { EmpresaService } from './empresa.service';
 import { EventoService } from './evento.service';
 import { VehiculoService } from './vehiculo.service';
+import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, HttpClientModule],
-  providers: [EmpresaService, EventoService, VehiculoService],
+  providers: [EmpresaService, EventoService, VehiculoService, AuthService],
 })
 export class CoreModule {}
